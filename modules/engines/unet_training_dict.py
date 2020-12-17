b44ea35e29db8c75b0f672d8f5bdfe17d729e531
Nic Ma <nma@nvidia.com>
2020-12-18 01:47:57 +0800

    [DLMED] fix minor issues in examples
    Signed-off-by: Nic Ma <nma@nvidia.com>

--- modules/engines/unet_training_dict.py
+++ modules/engines/unet_training_dict.py
@@ -139,7 +139,7 @@ def main(tempdir):
         additional_metrics={"val_acc": Accuracy(output_transform=lambda x: (x["pred"], x["label"]))},
         val_handlers=val_handlers,
         # if no FP16 support in GPU or PyTorch version < 1.6, will not enable AMP evaluation
-        amp=True if monai.config.get_torch_version_tuple() >= (1, 6) else False,
+        amp=True if monai.utils.get_torch_version_tuple() >= (1, 6) else False,
     )
 
     train_post_transforms = Compose(
@@ -169,7 +169,7 @@ def main(tempdir):
         key_train_metric={"train_acc": Accuracy(output_transform=lambda x: (x["pred"], x["label"]))},
         train_handlers=train_handlers,
         # if no FP16 support in GPU or PyTorch version < 1.6, will not enable AMP training
-        amp=True if monai.config.get_torch_version_tuple() >= (1, 6) else False,
+        amp=True if monai.utils.get_torch_version_tuple() >= (1, 6) else False,
     )
     trainer.run()
 

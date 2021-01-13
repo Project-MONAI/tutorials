d46ad99466729740c3a7e7f4ec286dcf48996c1b
Richard Brown <33289025+rijobro@users.noreply.github.com>
2021-01-13 15:21:28 +0000

    autopep8

--- runner.sh
+++ runner.sh
@@ -11,6 +11,90 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# output formatting
+separator=""
+blue=""
+green=""
+red=""
+noColor=""
+
+if [[ -t 1 ]] # stdout is a terminal
+then
+    separator=$'--------------------------------------------------------------------------------\n'
+    blue="$(tput bold; tput setaf 4)"
+    green="$(tput bold; tput setaf 2)"
+    red="$(tput bold; tput setaf 1)"
+    noColor="$(tput sgr0)"
+fi
+
+doAutopep8=true
+doRun=true
+autofix=false
+
+function print_usage {
+    echo "runtests.sh [--no-run] [--no-autopep8] [--autofix] [--help] [--version]"
+    echo ""
+    echo "MONAI tutorials testing utilities."
+    echo ""
+    echo "Examples:"
+    echo "./runtests.sh                             # run full tests (${green}recommended before making pull requests${noColor})."
+    echo "./runtests.sh --no-run                    # don't run the notebooks."
+    echo "./runtests.sh --no-autopep8               # don't run the autopep8."
+    echo "./runtests.sh --autofix                   # automatically fix autopep8 problems."
+    echo ""
+    echo "Code style check options:"
+    echo "    --no-run          : don't run notebooks"
+    echo "    --no-autopep8     : don't run \"autopep8\" code format checks"
+    echo "    --autofix         : automatically fix autopep8 problems"
+    echo "    -h, --help        : show this help message and exit"
+    echo "    -v, --version     : show MONAI and system version information and exit"
+    echo ""
+    echo "${separator}For bug reports, questions, and discussions, please file an issue at:"
+    echo "    https://github.com/Project-MONAI/MONAI/issues/new/choose"
+    echo ""
+    exit 1
+}
+
+function print_version {
+    ${cmdPrefix}${PY_EXE} -c 'import monai; monai.config.print_config()'
+}
+
+function print_style_fail_msg() {
+    echo "${red}Check failed!${noColor}"
+    echo "Please run auto style fixes: ${green}./runner.sh --autofix${noColor}"
+}
+
+# parse arguments
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+        --no-run)
+            doRun=false
+        ;;
+        --no-autopep8)
+            doAutopep8=false
+        ;;
+        --autofix)
+            autofix=true
+        ;;
+        -h|--help)
+            print_usage
+        ;;
+        -v|--version)
+            print_version
+            exit 1
+        ;;
+        *)
+            print_error_msg "Incorrect commandline provided, invalid key: $key"
+            print_usage
+        ;;
+    esac
+    shift
+done
+
+
+
 base_path="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 function replace_text {
@@ -31,7 +115,7 @@ function replace_text {
 files=()
 
 # Tested -- working
-# files=("${files[@]}" modules/load_medical_images.ipynb)
+files=("${files[@]}" modules/load_medical_images.ipynb)
 # files=("${files[@]}" modules/autoencoder_mednist.ipynb)
 # files=("${files[@]}" modules/integrate_3rd_party_transforms.ipynb)
 # files=("${files[@]}" modules/transforms_demo_2d.ipynb)
@@ -49,20 +133,18 @@ files=()
 # files=("${files[@]}" 3d_classification/torch/densenet_training_array.ipynb)
 # files=("${files[@]}" 3d_segmentation/spleen_segmentation_3d.ipynb)
 # files=("${files[@]}" 3d_segmentation/spleen_segmentation_3d_lightning.ipynb)
+# files=("${files[@]}" acceleration/transform_speed.ipynb)
 
 # Currently testing
+# files=("${files[@]}" acceleration/automatic_mixed_precision.ipynb)
 
 # Tested -- requires update
 # files=("${files[@]}" modules/dynunet_tutorial.ipynb)
-
-# Tested -- not working
-# files=("${files[@]}" acceleration/transform_speed.ipynb)
 
 # Not tested
 # files=("${files[@]}" 3d_segmentation/brats_segmentation_3d.ipynb)
 # files=("${files[@]}" 3d_segmentation/unet_segmentation_3d_catalyst.ipynb)
 # files=("${files[@]}" 3d_segmentation/unet_segmentation_3d_ignite.ipynb)
-# files=("${files[@]}" acceleration/automatic_mixed_precision.ipynb)
 # files=("${files[@]}" acceleration/dataset_type_performance.ipynb)
 # files=("${files[@]}" acceleration/fast_training_tutorial.ipynb)
 # files=("${files[@]}" acceleration/multi_gpu_test.ipynb)
@@ -70,7 +152,7 @@ files=()
 # files=("${files[@]}" modules/interpretability/class_lung_lesion.ipynb)
 
 for file in "${files[@]}"; do
-	echo -e "\nRunning $file"
+	echo "${separator}${blue}Running $file${noColor}"
 
 	# Get to file's folder and get file contents
 	path="$(dirname "${file}")"
@@ -78,20 +160,54 @@ for file in "${files[@]}"; do
 	cd ${base_path}/${path}
 	notebook=$(cat "$filename")
 
-	# Set some variables to 1 to speed up proceedings
-	strings_to_replace=(max_num_epochs max_epochs val_interval disc_train_interval disc_train_steps)
-	for s in "${strings_to_replace[@]}"; do
-		replace_text
-	done
+	########################################################################
+	#                                                                      #
+	#  autopep8                                                            #
+	#                                                                      #
+	########################################################################
+	if [ $doAutopep8 = true ]; then
+		if [ $autofix = false ]; then
+			out=$(autopep8 --list-fixes "$filename")
+			if [ -n "$out" ]; then
+				echo "$out"
+				success=1
+			fi
+		else
+			autopep8 -i "$filename"
+			success=$?
+		fi
 
-	jupyter nbconvert "$filename" --stdout --to script | flake8 -  #--ignore=W391
+	    if [ ${success} -ne 0 ]
+	    then
+	        print_style_fail_msg
+	        exit ${success}
+	    else
+	        echo "${green}passed!${noColor}"
+	    fi
+	fi
 
-	# Run with nbconvert
-	# echo "$notebook" > "${base_path}/debug_notebook.ipynb"
-	# out=$(echo "$notebook" | papermill --progress-bar)
-	# res=$?
-	# if [ $res -ne 0 ]; then
-	# 	echo -e $out
-	# 	exit $res
-	# fi
+	########################################################################
+	#                                                                      #
+	#  run notebooks with papermill                                        #
+	#                                                                      #
+	########################################################################
+	if [ $doRun = true ]; then
+
+		# Set some variables to 1 to speed up proceedings
+		strings_to_replace=(max_num_epochs max_epochs val_interval disc_train_interval disc_train_steps)
+		for s in "${strings_to_replace[@]}"; do
+			replace_text
+		done
+		
+		# echo "$notebook" > "${base_path}/debug_notebook.ipynb"
+		out=$(echo "$notebook" | papermill --progress-bar)
+		success=$?
+	    if [ ${success} -ne 0 ]
+	    then
+	        print_style_fail_msg
+	        exit ${success}
+	    else
+	        echo "${green}passed!${noColor}"
+	    fi
+	fi
 done

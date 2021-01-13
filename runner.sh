41023c2029905562bc1c490e21ba429f7b80ad95
Richard Brown <33289025+rijobro@users.noreply.github.com>
2021-01-13 11:18:36 +0000

    current progress

--- runner.sh
+++ runner.sh
@@ -11,23 +11,36 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Exit on error
-set -e
+base_path="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+function replace_text {
+	oldString="${s}\s*=\s*[0-9]\+"
+	newString="${s} = 1"
+
+	before=$(echo "$notebook" | grep "$oldString")
+	[ ! -z "$before" ] && echo Before: && echo "$before"
+
+	notebook=$(echo "$notebook" | sed "s/$oldString/$newString/g")
+	
+	after=$(echo "$notebook" | grep "$newString")
+	[ ! -z "$after"  ] && echo After: && echo "$after"
+}
 
 # TODO: replace this with:
 # find . -type f \( -name "*.ipynb" -and -not -iwholename "*.ipynb_checkpoints*" \)
 files=()
 
 # Tested -- working
-#files=("${files[@]}" modules/load_medical_images.ipynb)
-#files=("${files[@]}" modules/autoencoder_mednist.ipynb)
-#files=("${files[@]}" modules/integrate_3rd_party_transforms.ipynb)
-#files=("${files[@]}" modules/transforms_demo_2d.ipynb)
-#files=("${files[@]}" modules/nifti_read_example.ipynb)
+# files=("${files[@]}" modules/load_medical_images.ipynb)
+# files=("${files[@]}" modules/autoencoder_mednist.ipynb)
+# files=("${files[@]}" modules/integrate_3rd_party_transforms.ipynb)
+# files=("${files[@]}" modules/transforms_demo_2d.ipynb)
+# files=("${files[@]}" modules/nifti_read_example.ipynb)
+# files=("${files[@]}" modules/post_transforms.ipynb)
+# files=("${files[@]}" modules/3d_image_transforms.ipynb)
 
 # Currently testing
-# files=("${files[@]}" modules/3d_image_transforms.ipynb)
-files=("${files[@]}" modules/post_transforms.ipynb)
+files=("${files[@]}" modules/public_datasets.ipynb)
 
 # Not tested
 # files=("${files[@]}" 2d_classification/mednist_tutorial.ipynb)
@@ -49,32 +62,27 @@ files=("${files[@]}" modules/post_transforms.ipynb)
 # files=("${files[@]}" modules/mednist_GAN_workflow_array.ipynb)
 # files=("${files[@]}" modules/mednist_GAN_workflow_dict.ipynb)
 # files=("${files[@]}" modules/models_ensemble.ipynb)
-# files=("${files[@]}" modules/public_datasets.ipynb)
 # files=("${files[@]}" modules/varautoencoder_mednist.ipynb)
 # files=("${files[@]}" modules/dynunet_tutorial.ipynb
 
 for file in "${files[@]}"; do
-	echo "Running $file"
+	echo -e "\nRunning $file"
 
-	notebook=$(cat "$file")
+	# Get to file's folder and get file contents
+	path="$(dirname "${file}")"
+	filename="$(basename "${file}")"
+	cd ${base_path}/${path}
+	notebook=$(cat "$filename")
 
-	# Set number of epochs to 1
-	oldString="max_num_epochs\s*=\s*[0-9]\+"
-	newString="max_num_epochs = 1"
-	echo -e "$notebook" | grep "$oldString"
-	notebook=$(echo "$notebook" | sed "s/$oldString/$newString/g")
-	echo -e "$notebook" | grep "$newString"
-
-	# Set validation interval to 1
-	oldString="val_interval\s*=\s*[0-9]\+"
-	newString="val_interval = 1"
-	echo -e "$notebook" | grep "$oldString"
-	notebook=$(echo "$notebook" | sed "s/$oldString/$newString/g")
-	echo -e "$notebook" | grep "$newString"
+	# Set some variables to 1 to speed up proceedings
+	strings_to_replace=("max_num_epochs" "max_epochs" "val_interval")
+	for s in "${strings_to_replace[@]}"; do
+		replace_text
+	done
 
 	# Run with nbconvert
-	# echo "$notebook" > debug_notebook.ipynb
-	echo "$notebook" | papermill --progress-bar --log-output
+	# echo "$notebook" > "${base_path}/debug_notebook.ipynb"
+	out=$(echo "$notebook" | papermill --progress-bar)
 	res=$?
 	if [ $res -ne 0 ]; then
 		echo -e $out

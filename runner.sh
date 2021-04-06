3356eeac3319952e6b2051e01caeb5b280dcb9e8
Eric Kerfoot <17726042+ericspod@users.noreply.github.com>
2021-04-06 17:04:19 +0100

    Deploy Example Notebooks (#164)
    * Added notebook on ThreadBuffer
    
    Signed-off-by: Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
    
    * Added notebook on ThreadBuffer
    
    Signed-off-by: Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
    
    * Updates ThreadBuffer Notebook
    
    Signed-off-by: Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
    
    * Adding deploy notebooks
    
    Signed-off-by: Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
    
    * Deploy notebooks
    
    Signed-off-by: Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
    
    * Deploy notebooks
    
    Signed-off-by: Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
    
    * Notebook fixes
    
    * Update mednist_classifier_ray.ipynb
    
    * Runner fix
    
    * Add exception
    
    * Tweak script

--- runner.sh
+++ runner.sh
@@ -27,6 +27,7 @@ doesnt_contain_max_epochs=("${doesnt_contain_max_epochs[@]}" transform_speed.ipy
 doesnt_contain_max_epochs=("${doesnt_contain_max_epochs[@]}" transforms_demo_2d.ipynb)
 doesnt_contain_max_epochs=("${doesnt_contain_max_epochs[@]}" nifti_read_example.ipynb)
 doesnt_contain_max_epochs=("${doesnt_contain_max_epochs[@]}" 3d_image_transforms.ipynb)
+doesnt_contain_max_epochs=("${doesnt_contain_max_epochs[@]}" mednist_classifier_ray.ipynb)
 
 
 # output formatting
@@ -50,6 +51,7 @@ doRun=true
 autofix=false
 failfast=false
 pattern="."
+kernelspec="python3"
 
 function print_usage {
     echo "runner.sh [--no-run] [--no-checks] [--autofix] [-f/--failfast] [-p/--pattern <regex pattern>] [-h/--help] [-v/--version]"
@@ -60,7 +62,8 @@ function print_usage {
     echo "./runner.sh                             # run full tests (${green}recommended before making pull requests${noColor})."
     echo "./runner.sh --no-run                    # don't run the notebooks."
     echo "./runner.sh --no-checks                 # don't run code checks."
-    echo "./runner.sh --pattern \"read|load\"     # check files with \"read\" or \"load\" in path."
+    echo "./runner.sh --pattern \"read|load\"   # check files with \"read\" or \"load\" in path."
+    echo "./runner.sh --kernelspec \"kernel\"   # Set the kernelspec value used to run notebooks, default is \"python3\"."
     echo ""
     echo "Code style check options:"
     echo "    --no-run          : don't run notebooks"
@@ -103,6 +106,10 @@ do
         ;;
         -p|--pattern)
             pattern="$2"
+            shift
+        ;;
+        -k|--kernelspec)
+            kernelspec="$2"
             shift
         ;;
         -h|--help)
@@ -277,7 +284,7 @@ for file in "${files[@]}"; do
 		done
 
 		# echo "$notebook" > "${base_path}/debug_notebook.ipynb"
-		time out=$(echo "$notebook" | papermill --progress-bar)
+		time out=$(echo "$notebook" | papermill --progress-bar -k "$kernelspec")
 		success=$?
 	    if [[ ${success} -ne 0 || "$out" =~ "\"status\": \"failed\"" ]]; then
 	        test_fail ${success}

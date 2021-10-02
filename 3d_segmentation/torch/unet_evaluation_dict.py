8bf71b023738220c606bb5fb64d81c706023941c
Nic Ma <nma@nvidia.com>
2021-10-02 23:31:00 +0800

    [DLMED] update to use 255 images
    Signed-off-by: Nic Ma <nma@nvidia.com>

--- 3d_segmentation/torch/unet_evaluation_dict.py
+++ 3d_segmentation/torch/unet_evaluation_dict.py
@@ -66,7 +66,7 @@ def main(tempdir):
     devices = [torch.device("cuda" if torch.cuda.is_available() else "cpu")]
     #devices = get_devices_spec(None)
     model = UNet(
-        dimensions=3,
+        spatial_dims=3,
         in_channels=1,
         out_channels=1,
         channels=(16, 32, 64, 128, 256),

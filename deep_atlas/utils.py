06247e79285103ccf278271ab2bc98e6cf2ad0c6
Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>
2022-02-16 10:33:30 -0500

    Fix vector field plotting in DeepAtlas tutorial
    - When plotting a vector field as a quiver, the arrow x and y components
    were swapped.
    - When visualizing a displacement vector field via a deformed grid, the
    inverse of the actual warping transformation was shown. This is because
    cartesian coordinate paths were being displaced, rather than image
    pixels.

--- deep_atlas/utils.py
+++ deep_atlas/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import monai
+import torch
 
 
 def preview_image(image_array, normalize_by="volume", cmap=None, figsize=(12, 12), threshold=None):
@@ -42,12 +43,19 @@ def preview_image(image_array, normalize_by="volume", cmap=None, figsize=(12, 12
 
 
 def plot_2D_vector_field(vector_field, downsampling):
-    """vector_field should be a tensor of shape (2,H,W)"""
+    """Plot a 2D vector field given as a tensor of shape (2,H,W).
+
+    The plot origin will be in the lower left.
+    Using "x" and "y" for the rightward and upward directions respectively,
+      the vector at location (x,y) in the plot image will have
+      vector_field[1,y,x] as its x-component and
+      vector_field[0,y,x] as its y-component.
+    """
     downsample2D = monai.networks.layers.factories.Pool['AVG', 2](
         kernel_size=downsampling)
     vf_downsampled = downsample2D(vector_field.unsqueeze(0))[0]
     plt.quiver(
-        vf_downsampled[0, :, :], vf_downsampled[1, :, :],
+        vf_downsampled[1, :, :], vf_downsampled[0, :, :],
         angles='xy', scale_units='xy', scale=downsampling,
         headwidth=4.
     )
@@ -87,28 +95,15 @@ def plot_2D_deformation(vector_field, grid_spacing, **kwargs):
     and plot an x-y grid warped by this deformation.
 
     vector_field should be a tensor of shape (2,H,W)
-        Note: vector_field spatial indices are swapped to match the conventions of imshow and quiver
-    kwargs are passed to matplotlib plotting
     """
-    # phi in the following line is the deformation mapping.
-    # Note that we are swapping the spatial x and y when we evaluate vector_field;
-    # the reason for this is that we want to match the the "matrix" or "image" style
-    # conventions used by matplotlib imshow and quiver, where the axis used for "rows"
-    # precedes the axis used for "columns"
-    # deformation mapping
-    def phi(pt): return pt + vector_field[:, pt[1], pt[0]].numpy()
-
-    _, xmax, ymax = vector_field.shape
-    xvals = np.arange(0, xmax, grid_spacing)
-    yvals = np.arange(0, ymax, grid_spacing)
-    for x in xvals:
-        pts = [phi(np.array([x, y])) for y in yvals]
-        pts = np.array(pts)
-        plt.plot(pts[:, 0], pts[:, 1], **kwargs)
-    for y in yvals:
-        pts = [phi(np.array([x, y])) for x in xvals]
-        pts = np.array(pts)
-        plt.plot(pts[:, 0], pts[:, 1], **kwargs)
+    _, H, W = vector_field.shape
+    grid_img = np.zeros((H,W))
+    grid_img[np.arange(0, H, grid_spacing),:]=1
+    grid_img[:,np.arange(0, W, grid_spacing)]=1
+    grid_img = torch.tensor(grid_img, dtype=vector_field.dtype).unsqueeze(0) # adds channel dimension, now (C,H,W)
+    warp = monai.networks.blocks.Warp(mode="bilinear", padding_mode="zeros")
+    grid_img_warped = warp(grid_img.unsqueeze(0), vector_field.unsqueeze(0))[0]
+    plt.imshow(grid_img_warped[0], origin='lower', cmap='gist_gray')
 
 
 def preview_3D_deformation(vector_field, grid_spacing, **kwargs):

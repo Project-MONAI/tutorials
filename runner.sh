6281535ed94d8212c2e4a3d401f8ae4a944bea5a
Wenqi Li <831580+wyli@users.noreply.github.com>
2022-06-14 06:39:33 +0100

    fixes script (#755)
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>

--- runner.sh
+++ runner.sh
@@ -40,6 +40,8 @@ doesnt_contain_max_epochs=("${doesnt_contain_max_epochs[@]}" 2d_inference_3d_vol
 doesnt_contain_max_epochs=("${doesnt_contain_max_epochs[@]}" resample_benchmark.ipynb)
 doesnt_contain_max_epochs=("${doesnt_contain_max_epochs[@]}" infoANDinference.ipynb)
 doesnt_contain_max_epochs=("${doesnt_contain_max_epochs[@]}" dice_loss_metric_notes.ipynb)
+doesnt_contain_max_epochs=("${doesnt_contain_max_epochs[@]}" 2d_slices_from_3d_sampling.ipynb)
+doesnt_contain_max_epochs=("${doesnt_contain_max_epochs[@]}" 2d_slices_from_3d_training.ipynb)
 
 # output formatting
 separator=""

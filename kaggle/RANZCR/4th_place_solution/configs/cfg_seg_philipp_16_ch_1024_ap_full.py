bff3af238d9ec3eacbe65e9931d852b74b13b94b
Yiheng Wang <68361391+yiheng-wang-nv@users.noreply.github.com>
2022-07-25 16:51:39 +0800

    update to support metatensor (#826)
    Signed-off-by: Yiheng Wang <vennw@nvidia.com>

--- kaggle/RANZCR/4th_place_solution/configs/cfg_seg_philipp_16_ch_1024_ap_full.py
+++ kaggle/RANZCR/4th_place_solution/configs/cfg_seg_philipp_16_ch_1024_ap_full.py
@@ -1,9 +1,7 @@
 import numpy as np
 from monai.transforms import (
-    CastToTyped,
     CenterSpatialCropd,
     Compose,
-    EnsureTyped,
     Lambdad,
     NormalizeIntensityd,
     RandAffined,
@@ -62,10 +60,8 @@ cfg.train_aug = Compose(
             max_spatial_size=(102, 102),
             prob=0.5,
         ),
-        CastToTyped(keys="input", dtype=np.float32),
         NormalizeIntensityd(keys="input", nonzero=False),
         Lambdad(keys="input", func=lambda x: x.clip(-20, 20)),
-        EnsureTyped(keys=("input", "mask")),
     ]
 )
 
@@ -82,10 +78,8 @@ cfg.val_aug = Compose(
         CenterSpatialCropd(
             keys=("input", "mask"), roi_size=(cfg.img_size[0], cfg.img_size[1])
         ),
-        CastToTyped(keys="input", dtype=np.float32),
         NormalizeIntensityd(keys="input", nonzero=False),
         Lambdad(keys="input", func=lambda x: x.clip(-20, 20)),
-        EnsureTyped(keys=("input", "mask")),
     ]
 )
 
@@ -99,9 +93,7 @@ cfg.test_aug = Compose(
             align_corners=False,
         ),
         SpatialPadd(keys=("input", "mask"), spatial_size=(1120, 1120)),
-        CastToTyped(keys="input", dtype=np.float32),
         NormalizeIntensityd(keys="input", nonzero=False),
         Lambdad(keys="input", func=lambda x: x.clip(-20, 20)),
-        EnsureTyped(keys=("input", "mask")),
     ]
 )

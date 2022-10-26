85f971fdac343898a1bcb39f62b04d8d8e3eb8ba
KumoLiu <yunl@nvidia.com>
2022-10-26 15:19:38 +0800

    use original repo split
    Signed-off-by: KumoLiu <yunl@nvidia.com>

--- pathology/hovernet/training_torch.py
+++ pathology/hovernet/training_torch.py
@@ -9,31 +9,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
-sys.path.append('/workspace/Code/tutorials/pathology/hovernet/transforms')
-sys.path.append('/workspace/Code/tutorials/pathology/hovernet/loss')
-from loss import HoVerNetLoss
-from transforms import (
-    GenerateWatershedMaskd,
-    GenerateInstanceBorderd,
-    GenerateDistanceMapd,
-    GenerateWatershedMarkersd,
-    Watershedd,
-    GenerateInstanceContour,
-    GenerateInstanceCentroid,
-    GenerateInstanceType,
-    GenerateInstanceCentroid, 
-    GenerateInstanceContour, 
-    GenerateInstanceType,
-)
-
 import os
+import math
 import time
 import torch
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel
 from torch.utils.tensorboard import SummaryWriter
 import numpy as np
 import pandas as pd
-from monai.data import DataLoader, decollate_batch, CacheDataset
+from argparse import ArgumentParser
+from monai.data import DataLoader, decollate_batch, CacheDataset, partition_dataset
 from monai.metrics import DiceMetric
 from monai.networks.nets import HoVerNet
 from monai.transforms import (
@@ -58,54 +44,51 @@ from monai.transforms import (
 from monai.utils import set_determinism, convert_to_tensor
 from monai.utils.enums import HoVerNetBranch
 from monai.visualize import plot_2d_or_3d_image
-from sklearn.model_selection import StratifiedShuffleSplit
+
+from loss import HoVerNetLoss
+from transforms import (
+    GenerateWatershedMaskd,
+    GenerateInstanceBorderd,
+    GenerateDistanceMapd,
+    GenerateWatershedMarkersd,
+    Watershedd,
+    GenerateInstanceContour,
+    GenerateInstanceCentroid,
+    GenerateInstanceType,
+    GenerateInstanceCentroid, 
+    GenerateInstanceContour, 
+    GenerateInstanceType,
+)
 
 
-
-def prepare_datasets(data_dir):
+def split_dataset(data_dir):
+    # using original split in the paper
     info = pd.read_csv(os.path.join(data_dir, "patch_info.csv"))
     file_names = np.squeeze(info.to_numpy()).tolist()
+    split_info = pd.read_csv(os.path.join(data_dir, "split_info.csv"))
 
-    img_sources = [v.split("-")[0] for v in file_names]
-    img_sources = np.unique(img_sources)
+    indices, splits = [], []
+    for i in range(3):
+        fold_case = split_info.loc[split_info['Split'] == i+1, 'Filename'].tolist()
+        fold_patches = [
+            file_name for file_name in file_names for _name in fold_case if _name == file_name.split("-")[0]
+        ]
+        fold_patches = np.unique(fold_patches)
+        print(f"Fold: {i} - {len(fold_patches):04d}")
 
-    cohort_sources = [v.split("_")[0] for v in img_sources]
-    _, cohort_sources = np.unique(cohort_sources, return_inverse=True)
+        fold_indices = [file_names.index(v) for v in fold_patches]
+        indices.append(fold_indices)
 
-    splitter = StratifiedShuffleSplit(n_splits=1, train_size=0.7, test_size=0.3, random_state=0)
+    for i in range(3):
+        _indices = indices.copy()
+        splits.append({
+            "valid": _indices.pop(i),
+            "train": sum(_indices, []),
+        })
 
-    split_generator = splitter.split(img_sources, cohort_sources)
-    for train_indices, valid_test_indices in split_generator:
-        train_cohorts = img_sources[train_indices]
-        valid_test_sources = img_sources[valid_test_indices]
-        valid_test_cohorts = cohort_sources[valid_test_indices]
+    return splits
 
-        _split_generator = splitter.split(valid_test_sources, valid_test_cohorts)
-        for valid_indices, test_indices in _split_generator:
-            valid_cohorts = valid_test_sources[valid_indices]
-            test_cohorts = valid_test_sources[test_indices]
-            train_names = [
-                file_name for file_name in file_names for source in train_cohorts if source == file_name.split("-")[0]
-            ]
-            valid_names = [
-                file_name for file_name in file_names for source in valid_cohorts if source == file_name.split("-")[0]
-            ]
-            test_names = [
-                file_name for file_name in file_names for source in test_cohorts if source == file_name.split("-")[0]
-            ]
-            train_names = np.unique(train_names)
-            valid_names = np.unique(valid_names)
-            test_names = np.unique(test_names)
-            print(f"Train: {len(train_names):04d} - Valid: {len(valid_names):04d} - Test: {len(test_names):04d}")
-            if np.intersect1d(train_names, valid_names).size != 0:
-                raise ValueError("Train and validation cohorts has an overlap.")
-            if np.intersect1d(valid_names, test_names).size != 0:
-                raise ValueError("Validation and test cohorts has an overlap.")
-
-            train_indices = [file_names.index(v) for v in train_names]
-            valid_indices = [file_names.index(v) for v in valid_names]
-            test_indices = [file_names.index(v) for v in test_names]
-
+def prepare_data(data_dir, fold, splits):
     images = np.load(os.path.join(data_dir, "images.npy"))
     labels = np.load(os.path.join(data_dir, "labels.npy"))
 
@@ -118,12 +101,10 @@ def prepare_datasets(data_dir):
         }
         for image, label in zip(images, labels)
     ]
-
-    train_data = [data[i] for i in train_indices]
-    valid_data = [data[i] for i in valid_indices]
-    test_data = [data[i] for i in test_indices]
-
-    return train_data, valid_data, test_data
+    train_data = [data[i] for i in splits[fold]['train']]
+    valid_data = [data[i] for i in splits[fold]['valid']]
+    
+    return train_data, valid_data
 
 def post_process(output, device, return_binary=True, return_centroids=False, output_classes=None):
     post_trans_seg = Compose([
@@ -175,7 +156,15 @@ def post_process(output, device, return_binary=True, return_centroids=False, out
     return (pred_inst, inst_info_dict, pred_inst_dict)
 
 
-def run(data_dir):
+def run(data_dir, fold, args):
+    print("[info] number of GPUs:", torch.cuda.device_count())
+    if torch.cuda.device_count() > 1:
+        dist.init_process_group(backend="nccl", init_method="env://")
+        world_size = dist.get_world_size()
+    else:
+        world_size = 1
+    print("[info] world_size:", world_size)
+
     train_transforms = Compose(
         [
             EnsureChannelFirstd(keys=("image", "label"), channel_dim=-1),
@@ -205,16 +194,31 @@ def run(data_dir):
         ]
     )
 
-    train_data, valid_data, test_data = prepare_datasets(data_dir)
+    splits = split_dataset(data_dir)
+    train_data, valid_data = prepare_data(data_dir, fold, splits)
+
+    if torch.cuda.device_count() > 1:
+        train_data = partition_dataset(data=train_data[fold], shuffle=True, num_partitions=world_size, even_divisible=True)[
+            dist.get_rank()
+        ]
+        if len(valid_data[fold]) < world_size:
+            valid_data[fold] = valid_data[fold] * math.ceil(float(world_size) / float(len(valid_data[fold])))
+
+        valid_data = partition_dataset(data=valid_data[fold], shuffle=False, num_partitions=world_size, even_divisible=False)[
+            dist.get_rank()
+        ]
+    print("train_files:", len(train_data))
+    print("val_files:", len(valid_data))
+
     train_ds = CacheDataset(data=train_data, transform=train_transforms,
                         cache_rate=1.0, num_workers=4)
     valid_ds = CacheDataset(data=valid_data, transform=val_transforms,
                         cache_rate=1.0, num_workers=4)
-    train_loader = DataLoader(train_ds, batch_size=8, num_workers=4, shuffle=True, pin_memory=True)
-    val_loader = DataLoader(valid_ds, batch_size=8, num_workers=4, pin_memory=True)
+    train_loader = DataLoader(train_ds, batch_size=args.batch_size, num_workers=4, shuffle=True, pin_memory=True)
+    val_loader = DataLoader(valid_ds, batch_size=args.batch_size, num_workers=4, pin_memory=True)
 
-
-    device = torch.device("cuda:0")
+    device = torch.device(f"cuda:{dist.get_rank()}") if torch.cuda.device_count() > 1 else torch.device("cuda:0")
+    torch.cuda.set_device(device)
     model = HoVerNet(
         mode="fast",
         in_channels=3,
@@ -223,30 +227,43 @@ def run(data_dir):
         norm="batch",
         dropout_prob=0.2,
     ).to(device)
+    if torch.cuda.device_count() > 1:
+        model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
     loss_function = HoVerNetLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, weight_decay=1e-5)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, betas=(0.9, 0.999), weight_decay=0.0)
     lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=25)
     dice_metric = DiceMetric(include_background=False, reduction="mean")
+    if torch.cuda.device_count() > 1:
+        model = DistributedDataParallel(model, device_ids=[device], find_unused_parameters=False)
 
-    max_epochs = 300
+    if args.amp:
+        from torch.cuda.amp import GradScaler, autocast
+
+        scaler = GradScaler()
+        if torch.cuda.device_count() == 1 or dist.get_rank() == 0:
+            print("[info] amp enabled")
+
+    max_epochs = args.max_epochs
     val_interval = 2
     best_metric = -1
     best_metric_epoch = -1
-    best_metrics_epochs_and_time = [[], []]
     epoch_loss_values = []
     metric_values = []
-    writer = SummaryWriter()
+    if torch.cuda.device_count() == 1 or dist.get_rank() == 0:
+        writer = SummaryWriter(comment=f'_{fold}')
 
     total_start = time.time()
     for epoch in range(max_epochs):
         epoch_start = time.time()
-        print("-" * 10)
-        print(f"epoch {epoch + 1}/{max_epochs}")
+        if torch.cuda.device_count() == 1 or dist.get_rank() == 0:
+            print("-" * 10)
+            print(f"epoch {epoch + 1}/{max_epochs}")
+
         model.train()
         epoch_loss = 0
+        loss_torch = torch.zeros(2, dtype=torch.float, device=device)
         step = 0
         for batch_data in train_loader:
-            step_start = time.time()
             step += 1
             inputs, label, label_type, hover_map = (
                 batch_data["image"].to(device),
@@ -261,20 +278,46 @@ def run(data_dir):
                 HoVerNetBranch.NC: label_type,
             }
             optimizer.zero_grad()
-            outputs = model(inputs)
-            loss = loss_function(outputs, labels)
-            loss.backward()
-            optimizer.step()
+            if args.amp:
+                with autocast():
+                    outputs = model(inputs)
+                    loss = loss_function(outputs, labels)
+                
+                scaler.scale(loss).backward()
+                scaler.unscale_(optimizer)
+                torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+                scaler.step(optimizer)
+                scaler.update()
+            else:
+                outputs = model(inputs)
+                loss = loss_function(outputs.float(), labels)
+
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+                optimizer.step()
+
             epoch_loss += loss.item()
+            loss_torch[0] += loss.item()
+            loss_torch[1] += 1.0
             epoch_len = len(train_ds) // train_loader.batch_size
-            print(f"{step}/{epoch_len}, train_loss: {loss.item():.4f}")
-            writer.add_scalar("train_loss", loss.item(), epoch_len * epoch + step)
-        lr_scheduler.step()
-        epoch_loss /= step
-        epoch_loss_values.append(epoch_loss)
-        print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
+            if torch.cuda.device_count() == 1 or dist.get_rank() == 0:
+                print(f"{step}/{epoch_len}, train_loss: {loss.item():.4f}")
+                writer.add_scalar("train_loss", loss.item(), epoch_len * epoch + step)
+            lr_scheduler.step()
+        
+        if torch.cuda.device_count() > 1:
+            dist.barrier()
+            dist.all_reduce(loss_torch, op=torch.distributed.ReduceOp.SUM)
+
+        loss_torch = loss_torch.tolist()
+        if torch.cuda.device_count() == 1 or dist.get_rank() == 0:
+            loss_torch_epoch = loss_torch[0] / loss_torch[1]
+            print(
+                f"epoch {epoch + 1} average loss: {loss_torch_epoch:.4f}"
+            )
 
         if (epoch + 1) % val_interval == 0:
+            torch.cuda.empty_cache()
             model.eval()
             with torch.no_grad():
                 for val_data in val_loader:
@@ -282,8 +325,8 @@ def run(data_dir):
                         val_data["image"].to(device),
                         val_data["label"].to(device),
                     )
-
-                    val_outputs = model(val_inputs)
+                    with torch.cuda.amp.autocast(enabled=args.amp):
+                        val_outputs = model(val_inputs)
                     val_outputs = [post_process(i, device=device)[0] for i in decollate_batch(val_outputs)]
                     val_label = [i for i in decollate_batch(val_label)]
 
@@ -296,33 +339,64 @@ def run(data_dir):
                 if metric > best_metric:
                     best_metric = metric
                     best_metric_epoch = epoch + 1
-                    best_metrics_epochs_and_time[0].append(best_metric)
-                    best_metrics_epochs_and_time[1].append(time.time() - total_start)
-                    torch.save(
-                        model.state_dict(),
-                        os.path.join(writer.log_dir, "best_metric_model.pth"),
-                    )
-                    print("saved new best metric model")
+                    if torch.cuda.device_count() == 1 or dist.get_rank() == 0:
+                        torch.save(
+                            model.state_dict(),
+                            os.path.join(writer.log_dir, f"best_metric_model{fold}.pth"),
+                        )
+                        print("saved new best metric model")
                 print(
                     f"current epoch: {epoch + 1} current mean dice: {metric:.4f}"
                     f"\nbest mean dice: {best_metric:.4f}"
                     f" at epoch: {best_metric_epoch}"
                 )
-                writer.add_scalar("val_mean_dice", metric, epoch + 1)
+                if torch.cuda.device_count() == 1 or dist.get_rank() == 0:
+                    writer.add_scalar("val_mean_dice", metric, epoch + 1)
                 plot_2d_or_3d_image(val_inputs, epoch + 1, writer, index=0, tag="image")
                 plot_2d_or_3d_image(val_label, epoch + 1, writer, index=0, tag="label")
                 plot_2d_or_3d_image(val_outputs, epoch + 1, writer, index=0, tag="output")
 
+                if torch.cuda.device_count() > 1:
+                    dist.barrier()
+            torch.cuda.empty_cache()
+
         print(f"time consuming of epoch {epoch + 1} is: {(time.time() - epoch_start):.4f}")
     total_time = time.time() - total_start
-    print(
-    f"train completed, best_metric: {best_metric:.4f} "
-    f"at epoch: {best_metric_epoch}"
-    f"total time: {total_time}")
-    writer.close()
+    if torch.cuda.device_count() == 1 or dist.get_rank() == 0:
+        print(
+        f"train completed, best_metric: {best_metric:.4f} "
+        f"at epoch: {best_metric_epoch}"
+        f"total time: {total_time}")
+        writer.flush()
+        writer.close()
+
+    if torch.cuda.device_count() > 1:
+        dist.destroy_process_group()
+
+def main():
+    parser = ArgumentParser(description="HoVerNet training torch pipeline")
+    parser.add_argument("--ngc", action="store_true", dest="ngc", help="use ngc")
+    parser.add_argument("--no-amp", action="store_false", dest="amp", help="deactivate amp")
+
+    parser.add_argument("--n", type=int, default=3, dest="n_fold", help="fold of cross validation")
+    parser.add_argument("--bs", type=int, default=8, dest="batch_size", help="batch size")
+    parser.add_argument("--ep", type=int, default=200, dest="max_epochs", help="max epochs")
+
+    args = parser.parse_args()
+    
+    set_determinism(seed=0)
+    import sys
+    if args.ngc:
+        data_dir = "/Lizard"
+        sys.path.append('/workspace/pathology/lizard/transforms')
+        sys.path.append('/workspace/pathology/lizard/loss')
+    else:
+        data_dir = "/workspace/Data/Lizard/Prepared"
+        sys.path.append('/workspace/Code/tutorials/pathology/hovernet/transforms')
+        sys.path.append('/workspace/Code/tutorials/pathology/hovernet/loss')
+
+    for i in range(args.n_fold):
+        run(data_dir, i, args)
 
 if __name__ == "__main__":
-
-    set_determinism(seed=0)
-    data_dir = "/workspace/Data/Lizard/Prepared"
-    run(data_dir)
+    main()

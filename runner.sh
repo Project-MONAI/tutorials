e5e06858fb5e16fcbc0a97050197f3c9e7b7e8e1
Nic Ma <nma@nvidia.com>
2022-11-16 11:30:58 +0800

    [DLMED] fix typo
    Signed-off-by: Nic Ma <nma@nvidia.com>

--- runner.sh
+++ runner.sh
@@ -81,7 +81,7 @@ pattern="-and -name '*' -and ! -wholename '*federated_learning*'\
  -and ! -wholename '*transforms_update_meta_data*'\
  -and ! -wholename '*video_seg*'\
  -and ! -wholename '*tcia_dataset*'\
- -and ! -wholename '*MRI_reconstruction*'
+ -and ! -wholename '*MRI_reconstruction*'"
 kernelspec="python3"
 
 function print_usage {

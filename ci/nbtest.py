1a300b90df3611ca217a79716861d7b138a0dfff
Mingxin Zheng <18563433+mingxin-zheng@users.noreply.github.com>
2023-01-12 10:06:13 +0800

    remove shebang
    Signed-off-by: Mingxin Zheng <18563433+mingxin-zheng@users.noreply.github.com>

--- ci/nbtest.py
+++ ci/nbtest.py
@@ -1,5 +1,3 @@
-#!/bin/bash
-#
 # Copyright (c) MONAI Consortium
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.

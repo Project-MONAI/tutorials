5a3f5ee739a89417020130c153efb9db077dccee
Yiheng Wang <68361391+yiheng-wang-nv@users.noreply.github.com>
2023-01-17 23:05:03 +0800

    1161 update files according to new contributing guide (#1162)
    Signed-off-by: Yiheng Wang <vennw@nvidia.com>
    
    Fixes #1161  .
    
    ### Description
    I checked and updated the following tutorials in `modules` according to
    new contributing guide:
    
    - `dynunet_pipeline`
    - `engines`
    - `generate_random_permutations`
    - `interpretability`
    - `3d_image_transforms.ipynb`
    - `autoencoder_mednist.ipynb`
    - `batch_output_transform.ipynb`
    - `benchmark_global_mutual_information.ipynb`
    - `compute_metric.py`
    - `cross_validation_models_ensemble.ipynb`
    - `csv_datasets.ipynb`
    
    ### Checks
    <!--- Put an `x` in all the boxes that apply, and remove the not
    applicable items -->
    - [x] Avoid including large-size files in the PR.
    - [x] Clean up long text outputs from code cells in the notebook.
    - [x] For security purposes, please check the contents and remove any
    sensitive info such as user names and private key.
    - [x] Ensure (1) hyperlinks and markdown anchors are working (2) use
    relative paths for tutorial repo files (3) put figure and graphs in the
    `./figure` folder
    - [x] Notebook runs automatically `./runner.sh -t <path to .ipynb file>`
    
    Signed-off-by: Yiheng Wang <vennw@nvidia.com>
    Co-authored-by: Mingxin Zheng <18563433+mingxin-zheng@users.noreply.github.com>

--- modules/dynunet_pipeline/commands/task08/finetune_multi_gpu.sh
+++ modules/dynunet_pipeline/commands/task08/finetune_multi_gpu.sh
@@ -1,3 +1,14 @@
+# Copyright (c) MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # train step 2, finetune with small learning rate
 # please replace the weight variable into your actual weight
 # since this task uses lr scheduler, please set the lr and max epochs

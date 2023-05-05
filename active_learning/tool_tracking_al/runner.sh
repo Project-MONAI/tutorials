5e6a5320a2b3c009c2150c5c7531ad39d3fddfeb
Vishwesh <vishwesh.nath@gmail.com>
2023-05-05 14:47:04 -0500

    Active Learning Tutorial for Tool Tracking Segmentation (#1340)
    Fixes # .
    
    ### Description
    A few sentences describing the changes proposed in this pull request.
    
    ### Checks
    <!--- Put an `x` in all the boxes that apply, and remove the not
    applicable items -->
    - [x] Avoid including large-size files in the PR.
    - [x] Clean up long text outputs from code cells in the notebook.
    - [ ] For security purposes, please check the contents and remove any
    sensitive info such as user names and private key.
    - [ ] Ensure (1) hyperlinks and markdown anchors are working (2) use
    relative paths for tutorial repo files (3) put figure and graphs in the
    `./figure` folder
    - [ ] Notebook runs automatically `./runner.sh -t <path to .ipynb file>`
    
    ---------
    
    Signed-off-by: Vishwesh Nath <vnath@nvidia.com>
    Co-authored-by: Vishwesh Nath <vnath@nvidia.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Mingxin Zheng <18563433+mingxin-zheng@users.noreply.github.com>

---
+++ active_learning/tool_tracking_al/runner.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Copyright (c) MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DATA_ROOT="/to/be/defined"
+JSON_PATH="/to/be/defined"
+LOG_DIR="/to/be/defined"
+
+# Full dataset baseline
+python active_learning.py --base_dir ${LOG_DIR}/all_data_iter5k --data_root ${DATA_ROOT} --json_path ${JSON_PATH} --seed 120 --active_iters 1 --dropout_ratio 0.2 --mc_number 10 --initial_pool 360 --queries 0 --strategy random --steps 5000 --val_interval 1 --batch_size 1 --val_batch_size 1 --lr 1e-4
+
+# Initial Pool 15 Queries 15 Random Strategy
+python active_learning.py --base_dir ${LOG_DIR}/random_i15_q15_iter5k --data_root ${DATA_ROOT} --json_path ${JSON_PATH} --seed 120 --active_iters 6 --dropout_ratio 0.2 --mc_number 10 --initial_pool 15 --queries 15 --strategy random --steps 5000 --val_interval 1 --batch_size 1 --val_batch_size 1 --lr 1e-4
+
+# Initial Pool 15 Queries 15 Variance Strategy
+python active_learning.py --base_dir ${LOG_DIR}/variance_i15_q15_iter5k --data_root ${DATA_ROOT} --json_path ${JSON_PATH} --seed 120 --active_iters 6 --dropout_ratio 0.2 --mc_number 10 --initial_pool 15 --queries 15 --strategy random --steps 5000 --val_interval 1 --batch_size 1 --val_batch_size 1 --lr 1e-4

a8c19eea00b93566b392b4fb8ee99baaaba62a7d
Vishwesh Nath <vnath@nvidia.com>
2023-05-11 18:40:25 -0500

    Minor fixes to active learning tutorial for tool tracking
    Signed-off-by: Vishwesh Nath <vnath@nvidia.com>

--- active_learning/tool_tracking_al/runner.sh
+++ active_learning/tool_tracking_al/runner.sh
@@ -16,10 +16,10 @@ JSON_PATH="/to/be/defined"
 LOG_DIR="/to/be/defined"
 
 # Full dataset baseline
-python active_learning.py --base_dir ${LOG_DIR}/all_data_iter5k --data_root ${DATA_ROOT} --json_path ${JSON_PATH} --seed 120 --active_iters 1 --dropout_ratio 0.2 --mc_number 10 --initial_pool 360 --queries 0 --strategy random --steps 5000 --val_interval 1 --batch_size 1 --val_batch_size 1 --lr 1e-4
+python active_learning.py --base_dir ${LOG_DIR}/all_data_iter5k --data_root ${DATA_ROOT} --json_path ${JSON_PATH} --seed 240 --active_iters 1 --dropout_ratio 0.2 --mc_number 10 --initial_pool 360 --queries 0 --strategy random --steps 5000 --val_interval 1 --batch_size 1 --val_batch_size 1 --lr 1e-4
 
-# Initial Pool 15 Queries 15 Random Strategy
-python active_learning.py --base_dir ${LOG_DIR}/random_i15_q15_iter5k --data_root ${DATA_ROOT} --json_path ${JSON_PATH} --seed 120 --active_iters 6 --dropout_ratio 0.2 --mc_number 10 --initial_pool 15 --queries 15 --strategy random --steps 5000 --val_interval 1 --batch_size 1 --val_batch_size 1 --lr 1e-4
+# Initial Pool 20 Queries 20 Random Strategy
+python active_learning.py --base_dir ${LOG_DIR}/random_i15_q15_iter5k --data_root ${DATA_ROOT} --json_path ${JSON_PATH} --seed 240 --active_iters 5 --dropout_ratio 0.2 --mc_number 10 --initial_pool 20 --queries 20 --strategy random --steps 5000 --val_interval 1 --batch_size 1 --val_batch_size 1 --lr 1e-4
 
-# Initial Pool 15 Queries 15 Variance Strategy
-python active_learning.py --base_dir ${LOG_DIR}/variance_i15_q15_iter5k --data_root ${DATA_ROOT} --json_path ${JSON_PATH} --seed 120 --active_iters 6 --dropout_ratio 0.2 --mc_number 10 --initial_pool 15 --queries 15 --strategy random --steps 5000 --val_interval 1 --batch_size 1 --val_batch_size 1 --lr 1e-4
+# Initial Pool 20 Queries 20 Variance Strategy
+python active_learning.py --base_dir ${LOG_DIR}/variance_i15_q15_iter5k --data_root ${DATA_ROOT} --json_path ${JSON_PATH} --seed 240 --active_iters 5 --dropout_ratio 0.2 --mc_number 10 --initial_pool 20 --queries 20 --strategy random --steps 5000 --val_interval 1 --batch_size 1 --val_batch_size 1 --lr 1e-4

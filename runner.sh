6a3082bf1cee4bcd38743d0bc47ffc41103f7ab8
Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
2023-09-19 16:43:02 +0100

    Let's just ignore these notebooks for now
    Signed-off-by: Eric Kerfoot <eric.kerfoot@kcl.ac.uk>

--- runner.sh
+++ runner.sh
@@ -115,6 +115,10 @@ skip_run_papermill=("${skip_run_papermill[@]}" .*mednist_classifier_ray*)  # htt
 skip_run_papermill=("${skip_run_papermill[@]}" .*TorchIO_MONAI_PyTorch_Lightning*)  # https://github.com/Project-MONAI/tutorials/issues/1324
 skip_run_papermill=("${skip_run_papermill[@]}" .*GDS_dataset*)  # https://github.com/Project-MONAI/tutorials/issues/1324
 skip_run_papermill=("${skip_run_papermill[@]}" .*learn2reg_nlst_paired_lung_ct.ipynb*)  # slow test
+skip_run_papermill=("${skip_run_papermill[@]}" .*01_bundle_intro.ipynb*)
+skip_run_papermill=("${skip_run_papermill[@]}" .*02_mednist_classification.ipynb*)
+skip_run_papermill=("${skip_run_papermill[@]}" .*03_mednist_classification_v2.ipynb*)
+skip_run_papermill=("${skip_run_papermill[@]}" .*04_integrating_code.ipynb*)
 
 # output formatting
 separator=""

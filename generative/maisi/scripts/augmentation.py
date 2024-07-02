52672f61d05ea844cec66f4fbede03b9582e2afb
Can-Zhao <volcanofly@gmail.com>
2024-07-02 22:42:00 +0000

    reformat
    Signed-off-by: Can-Zhao <volcanofly@gmail.com>

--- generative/maisi/scripts/augmentation.py
+++ generative/maisi/scripts/augmentation.py
@@ -21,8 +21,6 @@ from torch import Tensor
 from .utils import dilate_one_img, erode_one_img
 
 MAX_COUNT = 100
-
-
 def initialize_tumor_mask(volume: Tensor, tumor_label: Sequence[int]) -> Tensor:
     """
     Initialize tumor mask for tumor augmentation.
@@ -39,10 +37,9 @@ def initialize_tumor_mask(volume: Tensor, tumor_label: Sequence[int]) -> Tensor:
         tumor_mask_[volume == label] = idx + 1
     return tumor_mask_
 
-
 def finalize_tumor_mask(distorted_mask: Tensor, organ_mask: Tensor, threshold_tumor_size: float):
     """
-    Try to generate the final tumor mask by combining the augmentd tumor mask and organ mask.
+    Try to generate the final tumor mask by combining the augmentd tumor mask and organ mask. 
     Need to make sure tumor is inside of organ and is larger than threshold_tumor_size.
 
     Args:
@@ -61,14 +58,17 @@ def finalize_tumor_mask(distorted_mask: Tensor, organ_mask: Tensor, threshold_tu
     else:
         return None
 
-
 def augmentation_bone_tumor(whole_mask: Tensor, spatial_size: tuple[int, int, int] | int | None = None) -> Tensor:
     """
     Bone tumor augmentation.
 
     Args:
         whole_mask: input 3D multi-label mask, [1,1,H,W,D] torch tensor.
-        spatial_size: output image spatial size, used in random transform. If not defined, will use (H,W,D). If some components are non-positive values, the transform will use the corresponding components of whole_mask size. For example, spatial_size=(128, 128, -1) will be adapted to (128, 128, 64) if the third spatial dimension size of whole_mask is 64.
+        spatial_size: output image spatial size, used in random transform. 
+                      If not defined, will use (H,W,D). If some components are non-positive values, 
+                      the transform will use the corresponding components of whole_mask size. 
+                      For example, spatial_size=(128, 128, -1) will be adapted to (128, 128, 64) 
+                      if the third spatial dimension size of whole_mask is 64.
 
     Return:
         augmented mask, with shape of spatial_size and data type as whole_mask.
@@ -144,7 +144,11 @@ def augmentation_liver_tumor(whole_mask: Tensor, spatial_size: tuple[int, int, i
 
     Args:
         whole_mask: input 3D multi-label mask, [1,1,H,W,D] torch tensor.
-        spatial_size: output image spatial size, used in random transform. If not defined, will use (H,W,D). If some components are non-positive values, the transform will use the corresponding components of whole_mask size. For example, spatial_size=(128, 128, -1) will be adapted to (128, 128, 64) if the third spatial dimension size of whole_mask is 64.
+        spatial_size: output image spatial size, used in random transform. 
+                      If not defined, will use (H,W,D). If some components are non-positive values, 
+                      the transform will use the corresponding components of whole_mask size. 
+                      For example, spatial_size=(128, 128, -1) will be adapted to (128, 128, 64) 
+                      if the third spatial dimension size of whole_mask is 64.
 
     Return:
         augmented mask, with shape of spatial_size and data type as whole_mask.
@@ -196,7 +200,7 @@ def augmentation_liver_tumor(whole_mask: Tensor, spatial_size: tuple[int, int, i
             tumor_mask = tumor_mask_
             # apply random augmentation
             distorted_mask = elastic((tumor_mask == 2), spatial_size=spatial_size).as_tensor()
-
+            
             # generate final tumor mask
             count += 1
             tumor_mask = finalize_tumor_mask(distorted_mask, organ_mask, tumor_size * 0.80)
@@ -218,7 +222,11 @@ def augmentation_lung_tumor(whole_mask: Tensor, spatial_size: tuple[int, int, in
 
     Args:
         whole_mask: input 3D multi-label mask, [1,1,H,W,D] torch tensor.
-        spatial_size: output image spatial size, used in random transform. If not defined, will use (H,W,D). If some components are non-positive values, the transform will use the corresponding components of whole_mask size. For example, spatial_size=(128, 128, -1) will be adapted to (128, 128, 64) if the third spatial dimension size of whole_mask is 64.
+        spatial_size: output image spatial size, used in random transform. 
+                      If not defined, will use (H,W,D). If some components are non-positive values, 
+                      the transform will use the corresponding components of whole_mask size. 
+                      For example, spatial_size=(128, 128, -1) will be adapted to (128, 128, 64) 
+                      if the third spatial dimension size of whole_mask is 64.
 
     Return:
         augmented mask, with shape of spatial_size and data type as whole_mask.
@@ -264,7 +272,8 @@ def augmentation_lung_tumor(whole_mask: Tensor, spatial_size: tuple[int, int, in
     assert 28 <= mode <= 32
     volume[tumor_mask_.bool()] = mode
     ###########################
-
+    
+    
     if tumor_size > 0:
         count = 0
         # get lung mask v2 (133 order)
@@ -305,7 +314,11 @@ def augmentation_pancreas_tumor(whole_mask: Tensor, spatial_size: tuple[int, int
 
     Args:
         whole_mask: input 3D multi-label mask, [1,1,H,W,D] torch tensor.
-        spatial_size: output image spatial size, used in random transform. If not defined, will use (H,W,D). If some components are non-positive values, the transform will use the corresponding components of whole_mask size. For example, spatial_size=(128, 128, -1) will be adapted to (128, 128, 64) if the third spatial dimension size of whole_mask is 64.
+        spatial_size: output image spatial size, used in random transform. 
+                      If not defined, will use (H,W,D). If some components are non-positive values, 
+                      the transform will use the corresponding components of whole_mask size. 
+                      For example, spatial_size=(128, 128, -1) will be adapted to (128, 128, 64) 
+                      if the third spatial dimension size of whole_mask is 64.
 
     Return:
         augmented mask, with shape of spatial_size and data type as whole_mask.
@@ -356,7 +369,7 @@ def augmentation_pancreas_tumor(whole_mask: Tensor, spatial_size: tuple[int, int
         while True:
             tumor_mask = tumor_mask_
             # apply random augmentation
-            distorted_mask = elastic((tumor_mask == 2), spatial_size=spatial_size).as_tensor()
+            distorted_mask = elastic((tumor_mask == 2), spatial_size=spatial_size).as_tensor()            
 
             # generate final tumor mask
             count += 1
@@ -379,7 +392,11 @@ def augmentation_colon_tumor(whole_mask: Tensor, spatial_size: tuple[int, int, i
 
     Args:
         whole_mask: input 3D multi-label mask, [1,1,H,W,D] torch tensor.
-        spatial_size: output image spatial size, used in random transform. If not defined, will use (H,W,D). If some components are non-positive values, the transform will use the corresponding components of whole_mask size. For example, spatial_size=(128, 128, -1) will be adapted to (128, 128, 64) if the third spatial dimension size of whole_mask is 64.
+        spatial_size: output image spatial size, used in random transform. 
+                      If not defined, will use (H,W,D). If some components are non-positive values, 
+                      the transform will use the corresponding components of whole_mask size. 
+                      For example, spatial_size=(128, 128, -1) will be adapted to (128, 128, 64) 
+                      if the third spatial dimension size of whole_mask is 64.
 
     Return:
         augmented mask, with shape of spatial_size and data type as whole_mask.
@@ -435,6 +452,7 @@ def augmentation_colon_tumor(whole_mask: Tensor, spatial_size: tuple[int, int, i
                 threshold = 0.75
             else:
                 break
+
 
             # generate final tumor mask
             count += 1

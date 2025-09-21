662e1224e47c4d74282798454a94bf47865e5d90
Eric Kerfoot <17726042+ericspod@users.noreply.github.com>
2025-09-21 22:53:48 +0100

    Updates to run notebook on cpu
    Signed-off-by: Eric Kerfoot <17726042+ericspod@users.noreply.github.com>

--- runner.sh
+++ runner.sh
@@ -466,6 +466,15 @@ trap finish EXIT
 # After setup, don't want to exit immediately after error
 set +e
 
+# FIXME: https://github.com/Project-MONAI/MONAI/issues/4354
+protobuf_major_version=$(${PY_EXE} -m pip list | grep '^protobuf ' | tr -s ' ' | cut -d' ' -f2 | cut -d'.' -f1)
+if [ "$protobuf_major_version" -ge "4" ]
+then
+    export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
+else
+    unset PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION
+fi
+
 ########################################################################
 #                                                                      #
 #  loop over files                                                     #
@@ -558,15 +567,6 @@ for file in "${files[@]}"; do
         done
 
         python -c 'import monai; monai.config.print_config()'
-
-        # FIXME: https://github.com/Project-MONAI/MONAI/issues/4354
-        protobuf_major_version=$(${PY_EXE} -m pip list | grep '^protobuf ' | tr -s ' ' | cut -d' ' -f2 | cut -d'.' -f1)
-        if [ "$protobuf_major_version" -ge "4" ]
-        then
-            export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
-        else
-            unset PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION
-        fi
 
         cmd=$(echo "papermill ${papermill_opt} --progress-bar --log-output -k ${kernelspec}")
         echo "$cmd"

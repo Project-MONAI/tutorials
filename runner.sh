713c6b23bc4543d0342592d58b96af1839e71a47
YunLiu <55491388+KumoLiu@users.noreply.github.com>
2025-09-22 19:13:25 +0800

    Skip tcia notebook test for 1.5.1 release (#2033)
    Workaround for #2029 
    
    ### Checks
    <!--- Put an `x` in all the boxes that apply, and remove the not
    applicable items -->
    - [ ] Avoid including large-size files in the PR.
    - [ ] Clean up long text outputs from code cells in the notebook.
    - [ ] For security purposes, please check the contents and remove any
    sensitive info such as user names and private key.
    - [ ] Ensure (1) hyperlinks and markdown anchors are working (2) use
    relative paths for tutorial repo files (3) put figure and graphs in the
    `./figure` folder
    - [ ] Notebook runs automatically `./runner.sh -t <path to .ipynb file>`
    
    Signed-off-by: Yun Liu <yunl@nvidia.com>

--- runner.sh
+++ runner.sh
@@ -131,6 +131,7 @@ skip_run_papermill=("${skip_run_papermill[@]}" .*unet_segmentation_3d_ignite_cle
 skip_run_papermill=("${skip_run_papermill[@]}" .*vista_2d_tutorial_monai.ipynb*)
 skip_run_papermill=("${skip_run_papermill[@]}" .*learn2reg_oasis_unpaired_brain_mr.ipynb*)
 skip_run_papermill=("${skip_run_papermill[@]}" .*finetune_vista3d_for_hugging_face_pipeline.ipynb*)
+skip_run_papermill=("${skip_run_papermill[@]}" .*TCIA_PROSTATEx_Prostate_MRI_Anatomy_Model.ipynb*)  # https://github.com/Project-MONAI/tutorials/issues/2029
 
 # output formatting
 separator=""
